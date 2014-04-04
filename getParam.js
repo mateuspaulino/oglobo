2ee4dc5e7b72168a7da3483ab89f6595c34daa15
ayrtonteshima <ayrtonteshima@gmail.com>
2014-04-04 18:25:56 -0300

    Função javascript getParam que pega a variável da url e retorna o seu valor

---
+++ getParam.js
@@ -0,0 +1,18 @@
+function getParam (key) {
+     var q = location.search.substring(1);
+     
+     if(q===undefined || q === null) 
+        return null;
+
+    var v = q.split("&");
+    var params = {};
+    var vn=[];
+
+    for (var i = 0; i < v.length; i++) {
+        vn=v[i].split('=');
+        if(vn[0]===key) {
+            return vn[1];
+        }
+    }
+    return null;
+ }
